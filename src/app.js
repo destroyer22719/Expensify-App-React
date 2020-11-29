347bdb33dfbcde9e2fdd117d7b1bd1bc3ac17453
Nathan <nathancai1031@gmail.com>
2020-11-29 15:11:37 -0500

    finished video

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<p>Hello World</p>, document.getElementById('app'))
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+  <div>This is the dashboard page</div>
+)
+
+const AddExpensePage = () => (
+  <div>This is the add expense page</div>
+)
+
+const EditPage = () => (
+  <div>This is the edit page</div>
+)
+
+const HelpPage = () => (
+  <div>This is the help page</div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage} exact={true}/>
+      <Route path="/edit" component={EditPage} exact={true}/>
+      <Route path="/help" component={HelpPage} exact={true}/>
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
