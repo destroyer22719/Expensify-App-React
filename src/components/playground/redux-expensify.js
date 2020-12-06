62e06fd24b3287c87399107f476a77e808d2200b
Nathan <nathancai1031@gmail.com>
2020-12-06 10:53:18 -0500

    done

--- src/components/playground/redux-expensify.js
+++ src/components/playground/redux-expensify.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const expensesDefault = []
 
+//expense reducer types
 const addExpense = ({
   description = '',
   note = '',
@@ -31,21 +32,21 @@ const editExpense =(id, updates) => ({
   updates
 })
 
-const demoState = {
-  expenses: [{
-    id: '1',
-    descriptor: 'January Rent',
-    note: 'This was the final payment for that address',
-    amount: 100000,
-    createdAt: 0
-  }],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount',
-    startDate: undefined,
-    endDate: undefined,
-  }
-};
+// const demoState = {
+//   expenses: [{
+//     id: '1',
+//     descriptor: 'January Rent',
+//     note: 'This was the final payment for that address',
+//     amount: 100000,
+//     createdAt: 0
+//   }],
+//   filters: {
+//     text: 'rent',
+//     sortBy: 'amount',
+//     startDate: undefined,
+//     endDate: undefined,
+//   }
+// };
 
 const expenseReducer = (state = expensesDefault, action) => {
   switch (action.type) {
@@ -85,14 +86,53 @@ const filterReducer = (state = filterDefault, action) => {
         ...state,
         text: action.text
       }
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date' 
+      }
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount' 
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.date 
+      }
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.date 
+      }
     default:
       return state;
   }
 }
 
+//filter reducer types
 const setTextFilter = (text) => ({ 
   type: 'SET_TEXT_FILTER',
   text,
+})
+
+const sortByAmount = () => ({ 
+  type: 'SORT_BY_AMOUNT',
+})
+
+const sortByDate = () => ({ 
+  type:'SORT_BY_DATE',
+})
+
+const setStartDate = date => ({
+  type: 'SET_START_DATE',
+  date,
+})
+
+const setEndDate = date => ({
+  type: 'SET_END_DATE',
+  date,
 })
 
 const store = createStore(combineReducers({
@@ -105,11 +145,16 @@ store.subscribe(() => {
   console.log(store.getState());
 })
 
+store.dispatch(setStartDate(123))
+store.dispatch(setEndDate(123))
 
-const Rent = store.dispatch(addExpense({descriptor: 'Rent', amount: 100}));
-const Hotel = store.dispatch(addExpense({descriptor: 'Hotel', amount: 1000}));
+// const Rent = store.dispatch(addExpense({descriptor: 'Rent', amount: 100}));
+// const Hotel = store.dispatch(addExpense({descriptor: 'Hotel', amount: 1000}));
 
-console.log(Hotel.expenses.id)
-store.dispatch(removeExpense({id: Hotel.expenses.id}))
-store.dispatch(editExpense(Rent.expenses.id, {amount: 500}));
-store.dispatch(setTextFilter('Another Rent'))
+// console.log(Hotel.expenses.id);
+// store.dispatch(removeExpense({id: Hotel.expenses.id}));
+// store.dispatch(editExpense(Rent.expenses.id, {amount: 500}));
+// store.dispatch(setTextFilter('Another Rent'));
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
