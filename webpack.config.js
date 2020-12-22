a97514991c8135345596fffdbfd15bb5bffafe21
Nathan <nathancai1031@gmail.com>
2020-12-22 10:44:13 -0500

    Finished Creating Seperate CSS Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = env => {
   const isProduction = env['production--mode'] === 'production';
-  console.log(isProduction);
+
   return {
+    plugins: [new MiniCssExtractPlugin()],
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js'
     },
     module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ]
-      }]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, 
+        {
+          test: /\.s?css$/,
+          use: [ MiniCssExtractPlugin.loader, 
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            { 
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            } 
+          ]
+        }
+      ]
     },
-    devtool: isProduction ? 'source-map' :'eval-cheap-module-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
