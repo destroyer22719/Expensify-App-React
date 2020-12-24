c86ad1c54ef47f9976498d943e2a595ef9598279
Nathan <nathancai1031@gmail.com>
2020-12-24 11:38:49 -0500

    done

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({ dispatch, description, id, amount, createdAt}) => (
   <div>
     <NavLink to={`/edit/${id}`}><h3>{description}</h3></NavLink>
-    <p>Amount: <span>{amount}</span> | Date: <span>{createdAt}</span></p>
+    <p>
+      Amount: <span>{numeral(amount / 100).format('$0,0.00')}</span> <br/> 
+      Date: <span>{moment(createdAt).format("dddd, MMMM Do, YYYY")}</span></p>
     <button onClick={() => {
       dispatch(removeExpense({id}));
     }}>Remove</button>
