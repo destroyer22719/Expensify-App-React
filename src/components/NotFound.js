803f3f244183071d7222e49d19c37d940fa48006
Nathan <nathancai1031@gmail.com>
2021-03-31 20:16:26 -0400

    Fixed formatting

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => (
-  <div>This is Not Found <br /> Go to <Link to="/">Go Home</Link> Page</div>
-)
+    <div>
+        This is Not Found <br /> Go to <Link to="/">Go Home</Link> Page
+    </div>
+);
 
-export default NotFound
+export default NotFound;
